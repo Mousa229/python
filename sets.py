8963e189911b74f466de2354bfec5c811374ae9e
Mousa <mosa.1994@hotmail.com>
2021-03-24 10:14:54 +0300

    sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+#In sets all the values need to be unique,and you can't repeat it.
+
+#Create an empty set
+s =set()
+
+# add elemnts to the set but you can not repeat them
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(3)
+s.add(4)
+s.remove(2)
+
+print(s)
