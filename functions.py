ae3f6e5af662e9fa3da906ff0565b757d6c4fbfc
Mousa <mosa.1994@hotmail.com>
2021-03-24 21:45:35 +0300

    using difination of our own making

---
+++ functions.py
@@ -0,0 +1,15 @@
+def square(x):
+    return x*x
+for i in range(10):
+
+
+#[for i in range(10)] :do some loop 10 times,
+# [print(f"The square of{i}]:and for each  time we loop  we print(The square of number is and plugging the value of (i)
+#[{square(x)}] and plugging the value of square(i) as an input.
+#Note: That you can use formated string (f {}  {} {} )and as many {} as you want for print or any function
+
+
+    print(f"The square of {i} is = {square(i)}")
+#also you can use it alone like this
+    print(f"{square(i)}")
+
