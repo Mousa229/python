624ee871529270e09a84ee70810a6a945d1a9352
Mousa <mosa.1994@hotmail.com>
2021-03-24 10:02:35 +0300

    list

---
+++ lists.py
@@ -0,0 +1,17 @@
+#Define a list of names.
+#in Lists you can change and modify the values or the elements of the list.
+
+names=["Harry","Ron","Hermione","Ginny"]
+
+#append used to add any name to the list
+
+names.append("Draco")
+
+#sort() used to sort the elements of list in order way
+
+names.sort()
+
+print(names)
+
+
+
