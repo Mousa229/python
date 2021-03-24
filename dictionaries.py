5c404228081a94380570d89093738c4b48816f0b
Mousa <mosa.1994@hotmail.com>
2021-03-24 19:52:54 +0300

    dictionarry

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+#The way we spasefay dictionaries is bay spasfaing a kea : value witch means> "Harry: Grefindor"
+houses = {"Harry":"Gryffindor","Draco":"Slytherin"}
+
+houses["Hermione"] = "Gryffindor"
+houses["Harry"]
+print(houses["Hermione"])
+print(houses["Harry"])
