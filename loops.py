ebf238126025f5f7a4e8ebeecf06ce4d532e432c
Mousa <mosa.1994@hotmail.com>
2021-03-24 19:19:44 +0300

    loops using for

---
+++ loops.py
@@ -0,0 +1,18 @@
+#for loop:  go through the list one element by one or one element at a time ,and for each one call it (i) .
+#You can use (i) or any letter or word
+#if you use (0,1,2,3,4,5) or [0,1,2,3,4,5,] or 0,1,2,3,4,5 all of them will give you the same results.
+for i in (0,1,2,3,4,5):
+    print(i)
+#Remember that in any function like range() or Print()
+##Remember that
+for i in range(7):
+    print(i)
+#You can use list of sequance in lops
+names=["Harry","Ron","Hermiony"]
+for name in names :
+    print(name)
+names="Harry"
+for character in names:
+    print(character)
+
+
